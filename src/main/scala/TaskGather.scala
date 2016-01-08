e7bcb460766effbb8964fbeeff851d20a9e0e8fe
ramirez7 <armando.m.ramirez@gmail.com>
2016-01-07 20:33:00 -0800

    convert tabs to spaces

--- src/main/scala/TaskGather.scala
+++ src/main/scala/TaskGather.scala
@@ -3,30 +3,30 @@ import scalaz.concurrent.Task
 import shapeless._
 
 trait TaskGather[In <: HList] {
-	type Out <: HList
+  type Out <: HList
 
-	def apply(l: In): Task[Out]
+  def apply(l: In): Task[Out]
 }
 
 object TaskGather {
-	type Aux[In <: HList, Out0 <: HList] = TaskGather[In] { type Out = Out0 }
+  type Aux[In <: HList, Out0 <: HList] = TaskGather[In] { type Out = Out0 }
 
-	implicit def hnilTaskGather: Aux[HNil, HNil] = 
-		new TaskGather[HNil] {
-			type Out = HNil
-			def apply(l: HNil): Task[Out] = HNil.point[Task]
-		}
+  implicit def hnilTaskGather: Aux[HNil, HNil] = 
+    new TaskGather[HNil] {
+      type Out = HNil
+      def apply(l: HNil): Task[Out] = HNil.point[Task]
+    }
 
-	implicit def hconsTaskGather[InH, InT <: HList](implicit gt: TaskGather[InT]): Aux[Task[InH] :: InT, InH :: gt.Out] = 
-		new TaskGather[Task[InH] :: InT] {
-			type Out = InH :: gt.Out
-			def apply(l: Task[InH] :: InT): Task[Out] = {
-				val gatheredTail = gt.apply(l.tail)
-				Nondeterminism[Task].mapBoth(l.head, gatheredTail){_ :: _}
-			}
-		}
+  implicit def hconsTaskGather[InH, InT <: HList](implicit gt: TaskGather[InT]): Aux[Task[InH] :: InT, InH :: gt.Out] = 
+    new TaskGather[Task[InH] :: InT] {
+      type Out = InH :: gt.Out
+      def apply(l: Task[InH] :: InT): Task[Out] = {
+        val gatheredTail = gt.apply(l.tail)
+        Nondeterminism[Task].mapBoth(l.head, gatheredTail){_ :: _}
+      }
+    }
 
-	implicit final class TaskGatherOps[L <: HList](val l: L) extends AnyVal {
-		def gather(implicit gt: TaskGather[L]): Task[gt.Out] = gt.apply(l)
-	}
+  implicit final class TaskGatherOps[L <: HList](val l: L) extends AnyVal {
+    def gather(implicit gt: TaskGather[L]): Task[gt.Out] = gt.apply(l)
+  }
 }
